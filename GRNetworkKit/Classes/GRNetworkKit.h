4047955e173474e2cf12bd84b90be48ccb4bd417
Grant Robinson <grant@zayda.com>
2016-10-06 16:17:12 -0600

    0.5.2 commit
    * added missing header file
    * bumped version to 0.5.2

--- GRNetworkKit/Classes/GRNetworkKit.h
+++ GRNetworkKit/Classes/GRNetworkKit.h
@@ -17,4 +17,5 @@ FOUNDATION_EXPORT const unsigned char GRNetworkKitVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <GRCoreData/PublicHeader.h>
 
 #import <GRNetworkKit/GRNetwork.h>
+#import <GRNetworkKit/GRHttpPost.h>
 
